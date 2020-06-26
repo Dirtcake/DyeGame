0e330ac49fc4ace66977bfaae4d439771102f60e
Victor <61842979+Dirtcake@users.noreply.github.com>
2020-06-26 19:26:29 -0300

    Estado 26/06

---
+++ Dye/Assets/Scripts/Prisma.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prisma : MonoBehaviour {
+    public GameObject enemy, projectile;
+
+    void Start () {
+
+    }
+
+    void Update () {
+        transform.LookAt (enemy.transform);
+
+        if (Timer (0.5f)) {
+            Instantiate (projectile, transform.position, transform.rotation);
+        }
+    }
+
+    private float tempo;
+    private bool Timer (float Intervalo) {
+
+        if (tempo < Time.time) {
+            tempo = Time.time + Intervalo;
+            return true;
+        } else return false;
+
+    }
+}
